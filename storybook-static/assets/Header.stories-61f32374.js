1be4709cad32c3e30eda4ec3ef06929c6f5eac08
Prashant Srivastav <srivastavp891@gmail.com>
2024-02-21 11:28:51 +0530

    re build with index.js

---
+++ storybook-static/assets/Header.stories-61f32374.js
@@ -0,0 +1,7 @@
+import{H as c}from"./Header-7fd8e70f.js";import"./jsx-runtime-29545a09.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-8d47fad6.js";import"./BasicButton-b6f00ad5.js";const H={title:"Example/Header",component:c,tags:["autodocs"],parameters:{layout:"fullscreen"}},e={args:{user:{name:"Jane Doe"}}},r={};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,L as __namedExportsOrder,H as default};
--- storybook-static/assets/Header.stories-ef9b7cc0.js
+++
@@ -1,7 +0,0 @@
-import{H as a}from"./Header-d9aac507.js";import"./BasicButton-9c0217aa.js";import"./index-ffb6278f.js";import"./_commonjsHelpers-725317a4.js";const c={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const m=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,m as __namedExportsOrder,c as default};
